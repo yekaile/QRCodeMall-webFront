07bfcdc0bf9ce09ded802eaedc8321729fda1cdc
jetbrains赛高 <2559659331@qq.com>
2020-09-03 11:52:34 +0800

    '把余改成了于'

--- src/components/common/Foot.vue
+++ src/components/common/Foot.vue
@@ -3,8 +3,8 @@
 
     <div>
         <span style=""> 一物一码自助商品溯源平台
-                    Copyright 叶凯乐，曹庚辰，涂清鹏，余子俊，刘晓婷
-        qq：2559659331，客服小乐在线陪聊</span>
+                    Copyright 叶凯乐，曹庚辰，涂清鹏，于子俊，刘晓婷
+        qq：2559659331，性感妹妹在线陪聊</span>
     </div>
 </template>
 
@@ -12,7 +12,9 @@
     export default {
         name: "Foot",
         data() {
-            return {}
+            return {
+
+            }
         },
 
     }
